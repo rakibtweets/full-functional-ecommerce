7c588a83520edd4175a236c5ba66a40636955425
rakibtweets <rakibhasanroky0@gmail.com>
2022-02-06 03:15:55 +0600

    cleaning the project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>React E-commerce</h1>
+      <h1 className=" text-primary fw-bold">React E-commerce</h1>
     </div>
   );
 }
