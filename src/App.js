d69db49c49977cc25a358c972664dc32de265b32
rakibtweets <rakibhasanroky0@gmail.com>
2022-02-07 23:12:13 +0600

    simple home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Components/Layout/Header/Header';
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <Container fluid>
+      <h1 id="products_heading">Latest Products</h1>
+      <section id="products" class="container mt-5">
+        <Row>
+          <div class="col-sm-12 col-md-6 col-lg-3 my-3">
+            <div class="card p-3 rounded">
+              <img
+                class="card-img-top mx-auto"
+                src="https://m.media-amazon.com/images/I/617NtexaW2L._AC_UY218_.jpg"
+                alt="Img"
+              />
+
+              <div class="card-body d-flex flex-column">
+                <h5 class="card-title">
+                  128GB Solid Storage Memory card - SanDisk Ultra
+                </h5>
+                <div class="ratings mt-auto">
+                  <div class="rating-outer">
+                    <div class="rating-inner"></div>
+                  </div>
+                  <span id="no_of_reviews">(5 Reviews)</span>
+                </div>
+                <p class="card-text">$45.67</p>
+                <button>View Details</button>
+              </div>
+            </div>
+          </div>
+        </Row>
+      </section>
+    </Container>
+  );
+};
+
+export default Home;
