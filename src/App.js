15f7966f93ada18c6e71783ad3f2a057c7527096
rakibtweets <rakibhasanroky0@gmail.com>
2022-02-07 22:06:34 +0600

    header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Header from './Components/Layout/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1 className=" text-primary fw-bold">React E-commerce</h1>
+      <Header />
     </div>
   );
 }
---
+++ src/Components/Layout/Header/Header.js
@@ -0,0 +1,46 @@
+import React, { Fragment } from 'react';
+import './Header.css';
+import { FaSearch } from 'react-icons/fa';
+
+const Header = () => {
+  return (
+    <Fragment>
+      <nav className="navbar row">
+        <div className="col-12 col-md-3">
+          <div className="navbar-brand">
+            <h6 className="text-white">React Shop</h6>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 mt-2 mt-md-0">
+          <div className="input-group">
+            <input
+              type="text"
+              id="search_field"
+              className="form-control"
+              placeholder="Enter Product Name ..."
+            />
+            <div className="input-group-append">
+              <button id="search_btn" className="btn">
+                <FaSearch />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 col-md-3 mt-4 mt-md-0 text-center">
+          <button className="btn" id="login_btn">
+            Login
+          </button>
+
+          <span id="cart" className="ms-3">
+            Cart
+          </span>
+          <span className="ms-1" id="cart_count">
+            2
+          </span>
+        </div>
+      </nav>
+    </Fragment>
+  );
+};
+
+export default Header;
